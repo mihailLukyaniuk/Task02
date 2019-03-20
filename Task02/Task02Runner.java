869c5905ef6cea9c2eac966ac5ac46464c04cfb9
mihailLukyaniuk <43263508+mihailLukyaniuk@users.noreply.github.com>
2019-03-20 07:06:37 +0300

    Add files via upload

---
+++ Task02/Task02Runner.java
@@ -0,0 +1,20 @@
+package Task02;
+
+public class Task02Runner {
+	public static void main(String args[]) {
+
+		DataTypesTester.testBoolean();
+		DataTypesTester.testByte();
+		DataTypesTester.testShort();
+		DataTypesTester.testInt();
+		DataTypesTester.testDouble();
+		DataTypesTester.testLong();
+		DataTypesTester.testFloat();
+		DataTypesTester.testChar();
+		DataTypesTester.testStudent();
+		DataTypesTester.testString();
+		
+		
+		
+	}
+}
